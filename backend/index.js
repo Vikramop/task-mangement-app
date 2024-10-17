f3590921591de4e1ac3b4f0ea182b319b38d3538
Vikramop <veervikramsingh132@gmail.com>
2024-10-17 20:17:20 +0530

    login and signup

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './db/connectDb.js';
-
+import authRoutes from './routes/auth.routes.js';
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // middleware, parses req.body
+
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
   connectDB();
